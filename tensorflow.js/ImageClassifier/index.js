d29fdc9bbe20fe2cbaf208df11b84bf33e8230c1
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-21 13:52:37 +0530

    stage 2

--- tensorflow.js/ImageClassifier/index.js
+++ tensorflow.js/ImageClassifier/index.js
@@ -1,3 +1,24 @@
 import * as tf from '@tensorflow/tfjs';
 import * as tfd from '@tensorflow/tfjs-data';
 
+import * as ui from './ui';
+
+const NUM_CLASSES = 4;
+
+let webcam;
+
+let truncatedMobileNet;
+let model;
+
+async function loadTruncatedMobileNet(){
+    const mobilenet = await tf.loadLayersModel(
+        'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json'
+    );
+    const layer = mobilenet.getLayer('conv_pw_13_relu');
+    return tf.model({inputs: mobilenet.inputs, outputs: layer.output});
+}
+
+ui.setExampleHandler(async label => {
+    let img = await getImage();
+    
+})
